e250021a0fbe40c23ad09b3fe1341a4da69a7925
Tugce Kiziltepe <b21946352@cs.hacettepe.edu.tr>
2022-04-14 17:51:01 +0300

    admin panel added and verify account function is working

---
+++ LinkedHU_CENG/Controllers/UnregisteredUserController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LinkedHU_CENG.Models;
+
+namespace LinkedHU_CENG.Controllers
+{
+    public class UnregisteredUserController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public UnregisteredUserController(ApplicationDbContext context)
+        {
+            this.db = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(db.UnregisteredUsers.ToList());
+        }
+
+        public IActionResult Register()
+        {
+            if (HttpContext.Session.GetString("UserID") == null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Register(UnregisteredUser usr)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UnregisteredUsers.Add(usr);
+                db.SaveChanges();
+                return RedirectToAction("Login", "User");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Some Error Occured!");
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            var post = db.UnregisteredUsers.Find(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            db.UnregisteredUsers.Remove(post);
+            db.SaveChanges();
+
+            return RedirectToAction("VerifyAccounts", "Administrator");
+        }
+    }
+}
---
+++ LinkedHU_CENG/Models/Administrator.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace LinkedHU_CENG.Models
+{
+    public class Administrator
+    {
+        [Key]
+        public string UserName { get; set; }
+
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
