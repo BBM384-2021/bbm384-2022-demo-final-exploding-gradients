5761d398ae9bb410970d527378982d9154da7b6c
Tugce Kiziltepe <tugcekzltp@outlook.com>
2022-04-14 06:10:58 +0300

    privacy footer is changed in layout that is shown after login

---
+++ LinkedHU_CENG/Migrations/20220413213107_migration1.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinkedHU_CENG.Migrations
+{
+    public partial class migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedAt",
+                table: "Posts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Posts",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Posts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedAt",
+                table: "Announcements",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Announcements",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Announcements",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Announcements");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Announcements");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Announcements");
+        }
+    }
+}
--- LinkedHU_CENG/Program.cs
+++ LinkedHU_CENG/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var connectionString = builder.Configuration.GetConnectionString("MertConnection");
+var connectionString = builder.Configuration.GetConnectionString("TugceConnection");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
