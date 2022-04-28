5af878257ccdd8c8fced670adc0594bb77693a6b
b21946055 <b21946055@cs.hacettepe.edu.tr>
2022-04-28 21:36:05 +0300

    Revert "demo"
    This reverts commit 7b8a5126319b0aa321f89939b551b1508a62fabb, reversing
    changes made to d7a16673696a21fe49bdd3f4b97e8ff4cb795dbb.

--- LinkedHU_CENG/Controllers/HomeController.cs
+++ LinkedHU_CENG/Controllers/HomeController.cs
@@ -15,14 +15,7 @@ namespace LinkedHU_CENG.Controllers
 
         public IActionResult Index()
         {
-            if (HttpContext.Session.GetString("UserID") == null)
-            {
-                return View("WelcomePage");
-            }
-            else
-            {
-                return View("LoggedIn"); ;
-            }
+            return View();
         }
 
         public IActionResult Privacy()
