dd2a0fdc813035864c3e637f54f4aa2b7445bc61
muratcelik <b21827263@cs.hacettepe.edu.tr>
2022-05-11 14:53:41 +0300

    version2

--- LinkedHU_CENG/Controllers/SearchController.cs
+++ LinkedHU_CENG/Controllers/SearchController.cs
@@ -15,54 +15,64 @@ namespace LinkedHU_CENG.Controllers
             this.db = context;
         }
 
-        
+
         public IActionResult Index()
         {
-            string word = HttpContext.Request.Form["word"];
-            
-            System.Diagnostics.Debug.WriteLine(word);
-            char[] charsToTrim = { '*', ' ', '\'' };
-            string[] wordSearch = word.Trim(charsToTrim).Split(" ");
-
-            string nameSurnameCommand = $"SELECT * FROM \"Users\" WHERE \"Name\" ILIKE '%{word}%'";
-            var nameSurnameList = db.Users.FromSqlRaw(nameSurnameCommand).ToList();
-
-            string nameCommand = $"SELECT * FROM \"Users\" WHERE \"Name\" ILIKE '%{wordSearch[0]}%'";
-            var nameList = db.Users.FromSqlRaw(nameCommand).ToList();
-            var surnameList = new List<User>();
-
-            if (wordSearch.Length > 1)
+            if (HttpContext.Session.GetInt32("UserID") != null)
             {
-                string surnameCommand = $"SELECT * FROM \"Users\" WHERE \"Surname\" ILIKE '%{wordSearch[1]}%'";
-                surnameList = db.Users.FromSqlRaw(surnameCommand).ToList();
-            }
+                string word = HttpContext.Request.Form["word"];
 
-            foreach (var usrNameSurname in nameSurnameList)
-            {
-                nameList.Remove(usrNameSurname);
-                surnameList.Remove(usrNameSurname);
-            }
-            foreach(var usrNameSurname in nameList)
-            {
-                surnameList.Remove(usrNameSurname);
-            }
+                System.Diagnostics.Debug.WriteLine(word);
+                char[] charsToTrim = { '*', ' ', '\'' };
+                string[] wordSearch = word.Trim(charsToTrim).Split(" ");
 
-            ViewData["NameSurname"] = nameSurnameList;
-            ViewData["Name"] = nameList;
-            ViewData["Surname"] = surnameList;
+                string nameSurnameCommand = $"SELECT * FROM \"Users\" WHERE \"Name\" ILIKE '%{word}%'";
+                var nameSurnameList = db.Users.FromSqlRaw(nameSurnameCommand).ToList();
 
-            return View();
+                string nameCommand = $"SELECT * FROM \"Users\" WHERE \"Name\" ILIKE '%{wordSearch[0]}%'";
+                var nameList = db.Users.FromSqlRaw(nameCommand).ToList();
+                var surnameList = new List<User>();
+
+                if (wordSearch.Length > 1)
+                {
+                    string surnameCommand = $"SELECT * FROM \"Users\" WHERE \"Surname\" ILIKE '%{wordSearch[1]}%'";
+                    surnameList = db.Users.FromSqlRaw(surnameCommand).ToList();
+                }
+
+                foreach (var usrNameSurname in nameSurnameList)
+                {
+                    nameList.Remove(usrNameSurname);
+                    surnameList.Remove(usrNameSurname);
+                }
+                foreach (var usrNameSurname in nameList)
+                {
+                    surnameList.Remove(usrNameSurname);
+                }
+
+                ViewData["NameSurname"] = nameSurnameList;
+                ViewData["Name"] = nameList;
+                ViewData["Surname"] = surnameList;
+
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
         }
 
 
         public IActionResult ViewProfile(int id)
         {
-            System.Diagnostics.Debug.WriteLine(id.ToString());
-            var user = db.Users.Find(id);
+            if (HttpContext.Session.GetInt32("UserID") != null)
+            {
+                System.Diagnostics.Debug.WriteLine(id.ToString());
+                var user = db.Users.Find(id);
+                List<Post> posts = db.Posts.OrderByDescending(x => x.CreatedAt).Where(x => x.UserId == id).ToList();
+                ViewData["SessionUserId"] = HttpContext.Session.GetInt32("UserID");
+                ViewData["User"] = user;
+                ViewData["Posts"] = posts;
 
-            ViewData["User"] = user;
-
-            return View("Profile");
+                return View("Profile");
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
