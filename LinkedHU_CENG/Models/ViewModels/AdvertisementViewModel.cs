48e0115bd64dc0a1be0cadddb8ba6965d9086f14
muratcelik <b21827263@cs.hacettepe.edu.tr>
2022-05-11 19:43:34 +0300

    bug fixed

--- LinkedHU_CENG/Models/ViewModels/AdvertisementViewModel.cs
+++ LinkedHU_CENG/Models/ViewModels/AdvertisementViewModel.cs
@@ -5,7 +5,6 @@
         public Advertisement Advertisement { get; set; }
         public Application Application { get; set; }
         public List<Application> Applications { get; set; }
-        public List<Certificate>? Certificates { get; set; }
         public int AdvertisementId { get; set; }
     }
 }
