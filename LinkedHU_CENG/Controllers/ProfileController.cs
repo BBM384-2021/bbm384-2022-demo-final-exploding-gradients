9a8f1960dc78c07cdec0f890802746a9b8e5fae9
b21946055 <b21946055@cs.hacettepe.edu.tr>
2022-05-12 23:56:29 +0300

    some changes

--- LinkedHU_CENG/Controllers/ProfileController.cs
+++ LinkedHU_CENG/Controllers/ProfileController.cs
@@ -1,6 +1,8 @@
 ﻿using LinkedHU_CENG.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace LinkedHU_CENG.Controllers
 {
@@ -130,8 +132,55 @@ namespace LinkedHU_CENG.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        public ActionResult ChangePassword()
+        {
+            if (HttpContext.Session.GetInt32("UserID") != null)
+            {
+                var user = db.Users.FirstOrDefault(u => u.UserId.Equals(HttpContext.Session.GetInt32("UserID")) && u.Email.Equals(HttpContext.Session.GetString("Email")));
 
-        
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                ViewData["User"] = user;
+
+                return View(user);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        [HttpPost]
+        public ActionResult ChangePasswordRequest(string oldPassword, string newPassword)
+        {
+            if (HttpContext.Session.GetInt32("UserID") != null)
+            {
+                var user = db.Users.FirstOrDefault(u => u.UserId.Equals(HttpContext.Session.GetInt32("UserID")) && u.Email.Equals(HttpContext.Session.GetString("Email")));
+                if (ModelState.IsValid)
+                {
+                    if (Encrypt(oldPassword) == user.Password)
+                    {
+                        user.Password = Encrypt(newPassword);
+                        db.SaveChanges();
+                        return RedirectToAction("Logout", "User");
+                    }
+                    else
+                    {
+                        // WARNING GELMELİ BURAYA
+                        return View();
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Some error occured!");
+                }
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
 
         private string UploadedFile(User user)
         {
@@ -154,6 +203,28 @@ namespace LinkedHU_CENG.Controllers
                 }
             }
             return uniqueFileName;
+        }
+
+        private string Encrypt(string clearText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
         }
     }
 }
